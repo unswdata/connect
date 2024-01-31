ed898431e6db71668d749fe1364963335ac942b8
oliviatripleu <wuolivia39@gmail.com>
2024-01-31 22:39:55 +1100

    made navbar

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar () {
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">
+                DataSoc Connect 
+            </a> 
+            <u1>
+                <li>
+                    <a href="/pending">Pending Connections</a> 
+                </li>
+                <li> 
+                    <a href="/past">Past Connections</a>
+                </li> 
+            </u1> 
+        </nav>
+    )
+  }
